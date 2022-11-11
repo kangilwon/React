d4ac432250b16ed7a9acdb8583c85476fbc03bcd
eleck11 <eleck11@naver.com>
2022-11-11 16:29:39 +0900

    main

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '블로그 제목 입니다.'
+  let [글제목, 글제목변경] = useState(['1번 제목', '2번 제목', '3번 제목']);
+  let [좋아요, 좋아요증가] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>blog</h4>
+      </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '뭐가문제냐'
+        글제목변경(copy);
+      }}>변경</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{ 좋아요증가(좋아요+1) }}>🤞</span> { 좋아요 } </h4>
+        <p>_월 _일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>_월 _일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>_월 _일 발행</p>
+      </div>
     </div>
   );
 }
